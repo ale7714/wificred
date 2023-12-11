810be9893945265c4c8472b9938af7b4e2a86ae5
Ale Paredes <aleu@viam.com>
2023-12-11 18:16:13 -0500

    captive flow

--- http-server/main.go
+++ http-server/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
+type CaptiveJson struct {
+	Captive       bool   `json:"captive"`
+	UserPortalUrl string `json:"user-portal-url"`
+}
+
+var captive CaptiveJson = CaptiveJson{
+	Captive:       true,
+	UserPortalUrl: "http://192.168.2.2/index.html",
+}
+
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf(r.Host, r.Body, r.Header, r.Method)
+		j, err := json.Marshal(captive)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Header().Set("Content-Type", "application/captive+json")
+		w.Write(j)
+	})
+
+	http.HandleFunc("/index.html", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf(r.Host, r.Body, r.Header, r.Method)
 
 		http.ServeFile(w, r, "./static/index.html")
